4115189099b9d8fb38ceff125cea7e87c8c531b8
sushankumar <sushankumar97@gmail.com>
2025-07-14 20:56:37 +0530

    commit

---
+++ Blind_Bidder_Program.py
@@ -0,0 +1,39 @@
+
+bidding = {}
+continue_bid = True
+
+def find_highest_bidder(bidding):
+    highestbid=0
+    for bid in bidding:
+        bid_amount=bidding[bid]
+        if bid_amount > highestbid:
+            highestbid = bid_amount
+            winner = bid
+    print(f"The winner is :{winner} ")
+
+while continue_bid:
+    print("Welcome to the secret auction Program:")
+    name = input("What is your name? \n")
+    bid = int(input("What is your Bid? \n"))
+    bidding[name] = bid
+    data = input("Are there any other bidders? Type 'yes' or 'no' \n")
+    if data == "no":
+        print("\n" * 29)
+        continue_bid = False
+        find_highest_bidder(bidding)
+    elif data == "yes":
+        print("\n"*29)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
